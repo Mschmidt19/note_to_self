274f3f36109194b479455e87c491b9e14b05b42c
Marek <MarekSchmidt@outlook.com>
2018-09-16 21:56:25 +0100

    First test passing

---
+++ src/components/Note.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Note from './Note'
+
+const props = { note: { text: 'test note' } }
+
+describe('Note', () => {
+  let note = mount(<Note {...props} />);
+  // let note = mount(<Note note={props.note} />);
+
+  it('renders the note text', () => {
+    expect(note.find('p').text()).toEqual(props.note.text);
+    // expect(note.find('p').text()).toEqual('test note');
+  });
+});
